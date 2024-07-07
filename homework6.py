f90d2348e250c7fc6a6d2e22bea6f988187ea6ae
DiasMS69 <Stigmat6.9.6@gmail.com>
2024-07-07 14:45:23 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict = {"Roshi": 453453, "Kilogo": 52384897, "Nikiloni": 841949558}
+print(my_dict)
+print(my_dict .get("Roshi"))
+print(my_dict .get("Lazer"))
+my_dict .update({"Lazer": 6238749, "Korizila": 24547457})
+print(my_dict)
+a = my_dict .pop("Roshi")
+print(a)
+my_set = {1, 2, 1, 2, 5, "Red", "Black", "Red"}
+print(my_set)
+print(my_set .update(["Rosy", "Lizard"]))
+print(my_set)
+print(my_set .remove(2))
+print(my_set)
+
+
